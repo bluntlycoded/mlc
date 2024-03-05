c3ae6ea8783aaa283a6ac8d62b6128740745ec5e
Rahul Nayak <114763168+bluntlycoded@users.noreply.github.com>
2024-03-06 04:49:14 +0530

    cms i hp

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,6 +17,7 @@ import getout from '../components/Images/Getout.png'
 import arcade from './Images/Arcade Zone.png'
 
 
+
 const Home = () => {
   let navigate = useNavigate(); 
   const AboutUs = () =>{ 
@@ -124,50 +125,48 @@ const Home = () => {
   return (
     <div className="home-container">
      
-      <nav style={{
-        color: "white",
-        fontFamily: "Poppins",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "1rem 2rem",
-        fontStyle: "bold",
-        marginTop: "2",
-        boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)", // Added box shadow for a modern touch
-      }} className={`navbar ${scrolling ? 'scrolling' : ''}`}>
-              {/* <img src={logo} alt="logo" style={{ width: "50px", height: "50px",padding:"0" }} className='nms' />
-              <br/> */}
-        <Link to="/" style={{ textDecoration: "none", color: "inherit", fontSize: "1.5rem", fontWeight: "bold",padding:"0px" }}>The Machine Learning Club</Link>
-        <div className="nav-links">
-          {/* {scrolling ? 'Scrolling' : 'Not Scrolling'} */}
-          {/* Desktop Links */}
-          <div className='desktop'>
-            <ul className="desktop-links" style={{ display: "flex", gap: "1rem" }}>
-              <li><Link to="/" style={{ textDecoration: "none", color: "white" }}>Home</Link></li>
-
-              <li><Link to="/about" style={{ textDecoration: "none", color: "white" }}>About Us</Link></li>
-              <li><Link to="/contact" style={{ textDecoration: "none", color: "white" }}>Contact Us</Link></li>
-              <li><Link to="/projects" style={{ textDecoration: "none", color: "white" }}>Projects</Link></li>
-              <li><Link to="/events" style={{ textDecoration: "none", color: "white" }}>Events</Link></li>
-              <li><Link to="/departments" style={{ textDecoration: "none", color: "white" }}>Departments</Link></li>
-            </ul>
-          </div>
-          {/* Mobile Menu */}
-          <div className='mobile'>
-            <div className="mobile-menu" style={{ display: mobileMenuOpen ? "block" : "none" }}>
-              
-              <ul style={{ listStyle: "none", padding: "0", margin: "0" }}>
-                <li><Link to="/about" style={{ textDecoration: "none", color: "white" }}>About Us</Link></li>
-                <li><Link to="/contact" style={{ textDecoration: "none", color: "white" }}>Contact Us</Link></li>
-                <li><Link to="/projects" style={{ textDecoration: "none", color: "white" }}>Projects</Link></li>
-                <li><Link to="/events" style={{ textDecoration: "none", color: "white" }}>Events</Link></li>
-                <li><Link to="/departments" style={{ textDecoration: "none", color: "white" }}>Departments</Link></li>
-              </ul>
-            </div>
-            <Link to="#" className="menu-icon" style={{ fontSize: "1.5rem", color: "white" }} onClick={toggleMobileMenu}>☰</Link>
-          </div>
+<nav style={{// Updated background color
+      color: "white",
+      fontFamily: "Poppins",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: "1rem 2rem",
+      fontStyle: "bold",
+      boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)", // Added box shadow for a modern touch
+    }} className={`navbar ${scrolling ? 'scrolling' : ''}`}>
+      <img src={logo} alt="logo" style={{ width: "50px", height: "50px" }} />
+      {/* <Link to="/" style={{ textDecoration: "none", color: "inherit", fontSize: "1.5rem", fontWeight: "bold" }}className='nms'>The Machine Learning Club</Link> */}
+      <div className="nav-links">
+      {/* {scrolling ? 'Scrolling' : 'Not Scrolling'} */}
+        {/* Desktop Links */}
+        <div className='desktop'>
+          <ul className="desktop-links" style={{ display: "flex", gap: "1rem" }}>
+            <li><Link to="/" style={{ textDecoration: "none", color: "white" }}>Home</Link></li>
+          
+            <li><Link to="/about" style={{ textDecoration: "none", color: "white" }}>About Us</Link></li>
+            <li><Link to="/contact" style={{ textDecoration: "none", color: "white" }}>Contact Us</Link></li>
+            <li><Link to="/projects" style={{ textDecoration: "none", color: "white" }}>Projects</Link></li>
+            <li><Link to="/events" style={{ textDecoration: "none", color: "white" }}>Events</Link></li>
+            <li><Link to="/departments" style={{ textDecoration: "none", color: "white" }}>Departments</Link></li>
+          </ul>
         </div>
-      </nav>
+        {/* Mobile Menu */}
+        <div className='mobile'>
+        <div className="mobile-menu" style={{ display: mobileMenuOpen ? "block" : "none" }}>
+          
+          <ul style={{ listStyle: "none", padding: "0", margin: "0" }}>
+            <li><Link to="/about" style={{ textDecoration: "none", color: "white" }}>About Us</Link></li>
+            <li><Link to="/contact" style={{ textDecoration: "none", color: "white" }}>Contact Us</Link></li>
+            <li><Link to="/projects" style={{ textDecoration: "none", color: "white" }}>Projects</Link></li>
+            <li><Link to="/events" style={{ textDecoration: "none", color: "white" }}>Events</Link></li>
+            <li><Link to="/departments" style={{ textDecoration: "none", color: "white" }}>Departments</Link></li>
+          </ul>
+        </div>
+        <Link to="#" className="menu-icon" style={{ fontSize: "1.5rem", color: "white" }} onClick={toggleMobileMenu}>☰</Link>
+        </div>
+      </div>
+    </nav>
       <section className="slider-section">
         <Slider images={images} />
       </section>
